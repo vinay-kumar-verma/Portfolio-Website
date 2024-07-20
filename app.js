2a3d73a7c21ef3926fd70186950cbf0ef47fa462
Vinay Kumar Verma <103379304+vinay-kumar-verma@users.noreply.github.com>
2024-07-20 21:35:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,16 @@
+const hamb = document.querySelector('.header .nav-bar .nav-list .hamb')
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul')
+const header = document.querySelector('.header.container')
+hamb.addEventListener('click', () => {
+    hamb.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+    })
+
+document,addEventListener('scroll', () =>{
+    var scroll_position = window.scrollY;
+    if(scroll_position > 250){
+        header.style.backgroundColor = '#29323c';
+    }else{
+        header.style.backgroundColor = 'transparent';
+    }
+})
